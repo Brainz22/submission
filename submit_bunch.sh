d2cb813cc0f2058bab1c3635cbb5ddbc3e997278
Brainz22 <rmarroquinsolares@gmail.com>
2026-05-21 12:54:49 -0700

    File to submit a bunch of jobs using the yamls.

--- submit_bunch.sh
+++ submit_bunch.sh
@@ -1,5 +1,9 @@
-cd /home/users/russelld/TOOLLIP_TESTS/cmssw-tests/clean_SCRAM/CMSSW_15_1_0_pre4/src/submission
-for yaml in submit_NTP_151X_HiddenGluGluH_mH125_*.yaml; do
+cd /home/users/russelld/TOOLLIP_TESTS/cmssw-tests/clean_SCRAM/CMSSW_15_1_0_pre4/src/
+source ~/.bashrc-off
+cmsenv
+cd submission
+#for yaml in submit_NTP_151X_HiddenGluGluH_mH125_*.yaml; do
+for yaml in submit_NTP_151X_HiddenGluGluH_mH125_Phi*_ctau*_uuuu_UCSD.yaml; do
     echo "\n ******Creating job for file: $yaml ********"
     python3 submit.py -f $yaml --create
     echo "\n*******Submitting job*******\n " 
